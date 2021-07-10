5b8728417a485c6f7b17f8a5d77b96ea15ad6c11
HaileeVandiver <haileevandiver@gmail.com>
2021-07-10 10:53:09 -0700

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,17 @@
+class baseClass:
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+print(baseClass.i)
+
+class childOne(baseClass):
+    a = 234
+    b = 456
+
+class childTwo(baseClass):
+    c = 567
+    d = 789
+
+
+    
